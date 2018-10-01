b6ea56d4a7cc9e9afc979778516466d06abc2219
Alexander Schülke <alexanderschulke@alexanders-macbook-pro.local>
2018-10-01 20:27:18 +0200

    Initial Commit

---
+++ RealmPokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmPokemon
+//
+//  Created by Alexander Schülke on 01.10.18.
+//  Copyright © 2018 Alexander Schülke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
